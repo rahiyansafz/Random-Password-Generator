aa9a78515a372af5c0b2129e8e2f027447570d52
rahiyansafz <rahiyansafin@gmail.com>
2022-04-13 00:28:52 +0600

    Add project files.

---
+++ RandomPasswordGenerator/App.cs
@@ -0,0 +1,27 @@
+﻿namespace RandomPasswordGenerator;
+
+internal class App
+{
+    public static void Run()
+    {
+        while (true)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Enter desired password length.");
+            int length = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Generated password of {0} character is", length);
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine(GeneratePassword(length));
+        }
+    }
+    private static string GeneratePassword(int passwordLength)
+    {
+        Random random = new();
+        char[] buffer = new char[passwordLength];
+        for (int i = 0; i < passwordLength; i++)
+            buffer[i] = (char)('a' + random.Next(0, 26));
+        var password = new string(buffer);
+
+        return password;
+    }
+}
---
+++ RandomPasswordGenerator/Program.cs
@@ -0,0 +1,3 @@
+﻿using RandomPasswordGenerator;
+
+App.Run();
